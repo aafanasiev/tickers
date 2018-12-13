67b9399aa96f7a6a783379e21d5e7f3097051c33
Afanasiev Aleksandr <afanasiev.aleksandr.92@gmail.com>
2018-12-13 22:56:25 +0200

    add new method

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,6 +3,10 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
 
+    func index(_ req: Request) throws -> Future<[Todo]> {
+        return Todo.query(on: req).all()
+    }
+    
     let todoController = TodoController()   
     router.get("getTickers", use: todoController.getTickers)
 }
